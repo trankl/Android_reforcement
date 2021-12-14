c4c2bd74cfb402c37c2dcb8e381d6e2ec126d67e
trankl <trankhanhly88@gmail.com>
2021-12-14 10:28:01 +0100

    Initial commit

---
+++ app/src/main/java/com/android_reforcement/MainActivity.java
@@ -0,0 +1,20 @@
+package com.android_reforcement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void act_voirTout(View view) {
+    }
+
+    public void act_ajouterMusicien(View view) {
+    }
+}
